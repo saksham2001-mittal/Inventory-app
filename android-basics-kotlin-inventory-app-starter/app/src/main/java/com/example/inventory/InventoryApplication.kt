de1dbe5a8a52fbc14340b6b2069bb4a15854e683
Saksham mittal <mittal.saksham2001@gmail.com>
2022-09-19 10:47:53 +0530

    sample2 commit

---
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.inventory
+
+import ItemRoomDatabase
+import android.app.Application
+
+
+//import com.example.inventory.data.ItemRoomDatabase
+
+class InventoryApplication : Application(){
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
+}
